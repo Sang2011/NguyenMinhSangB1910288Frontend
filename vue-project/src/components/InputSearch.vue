d3c73b02a279a93a7c3597e31755d4a24a34fd42
Sang2011 <sang201101071001@gmail.com>
2023-03-23 00:14:43 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ vue-project/src/components/InputSearch.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        props: {
+            modelValue: { type: String, default: "" },
+        },
+        emits: ["submit", "update: modelvalue"],
+        methods: {
+            updateModelValue(e) {
+                this.$emit("update: modelvalue", e.target.value);
+            },
+            submit() {
+                this.$emit("submit");
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="input-group">
+        <input 
+            type="text"
+            class="form-control"
+            placeholder="Nhap thong tin can tim"
+            :value="modelvalue"
+            @input="updateModelValue"
+            @keyup.enter="submit"
+            >
+
+            <div class="input-group-append">
+                <button 
+                class="btn btn-outline-secondary"
+                type="button"
+                @click="submit"
+                >
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+    </div>
+</template>
